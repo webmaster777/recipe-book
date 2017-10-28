d1ea076643d60c5b477d335ec418d7152896fee4
Meindert-Jan Kroese <meindertjan@gmail.com>
2017-10-28 20:27:04 +0200

    some comments on why this file is neccesary

--- cli-config.php
+++ cli-config.php
@@ -1,6 +1,11 @@
 <?php
 
-// replace with file to your own project bootstrap
-$container = require_once 'index.php';
+/**
+ * This file is required to get the doctrine cli working.
+ *
+ * All the magic happens in the
+ * container config (config.php)
+ */
 
+$container = require_once 'index.php';
 return $container->get('doctrine.orm.helperset');
