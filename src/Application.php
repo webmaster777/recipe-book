e7e8d1d5024ece881423db9b7eb7c8e535973869
Meindert-Jan Kroese <meindertjan@gmail.com>
2017-10-28 11:57:07 +0200

    first version of Application class + output in web app

---
+++ src/Application.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Mkroese\RecipeBook;
+
+class Application {
+
+  protected $name;
+
+  public function __construct($name = "Meindert-Jan's Recipe Book", $version = null) {
+    $this->name = $name;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  protected $versionParts, $fullVersion;
+
+  protected function parseVersion() {
+    $this->fullVersion = `git describe --tags --always`;
+    $versionParts = ["major" => 0, "minor" => 0, "patch" => 0, "prerelease" => "", "build" => ""];
+
+    if(preg_match('/^v?(\d+)\.(\d+)\.(\d+)-?([^+]*)+?(.*)$/',$this->fullVersion, $matches)) {
+      $versionParts["major"] = $matches[1];
+      $versionParts["minor"] = $matches[2];
+      $versionParts["patch"] = $matches[3];
+      $versionParts["prerelease"] = $matches[4];
+      $versionParts["build"] = $matches[5];
+    }
+    else {
+      $versionParts["build"] = $this->fullVersion;
+    }
+  }
+
+  public function getFullVersion() {
+    if(!$this->fullVersion) {
+      $this->parseVersion();
+    }
+    return $this->fullVersion;
+  }
+
+  public function getVersionPart($part) {
+    if(!$this->versionParts) {
+      $this->parseVersion();
+    }
+
+    if(array_key_exists($part, $this->versionParts)) {
+      return $this->versionParts[$part];
+    }
+
+    throw new \InvalidArgumentException("part must be one of " . var_export(array_keys($this->versionParts), true));
+  }
+
+}
--- web/index.php
+++ web/index.php
@@ -2,4 +2,12 @@
 
 // main entry point of the application
 
-echo "hello recipe-book";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+echo "hello recipe-book (autoload ='" .  __DIR__ . '/../vendor/autoload.php' . "')<br />" . PHP_EOL;
+
+$app = new \Mkroese\RecipeBook\Application();
+
+printf('This is "%s" version %s', $app->getName() ,$app->getFullVersion());
+
+echo PHP_EOL;
