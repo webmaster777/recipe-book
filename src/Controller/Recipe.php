a55076ee5bdf01fdd85e17ec9740b78465a151d2
Meindert-Jan Kroese <meindertjan@gmail.com>
2017-10-29 09:39:49 +0100

    created an edit-page: V+C

--- src/Controller/Recipe.php
+++ src/Controller/Recipe.php
@@ -10,13 +10,61 @@ namespace Mkroese\RecipeBook\Controller;
 
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityNotFoundException;
+use Mkroese\RecipeBook\Application;
 use \Mkroese\RecipeBook\Entity\Recipe as RecipeEntity;
 
 class Recipe
 {
-  public function getRecipeList(EntityManager $em)
+  protected $repository, $enitityManager, $application, $twig;
+
+  public function __construct(Application $application, EntityManager $entityManager, \Twig_Environment $twig)
   {
-    $repository = $em->getRepository(RecipeEntity::class);
-    return $repository->findAll();
+    $this->enitityManager = $entityManager;
+    $this->repository = $entityManager->getRepository(RecipeEntity::class);
+    $this->application = $application;
+    $this->twig = $twig;
+  }
+
+  public function editRecipe($id = null)
+  {
+    if($id === null) {
+      $entity = new RecipeEntity();
+    }
+    else {
+      $entity = $this->repository->find($id);
+    }
+
+    if(!$entity)
+      throw new EntityNotFoundException('Recipe not found!');
+
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+      $entity->title = $_POST["title"];
+
+      // Save recipe to the database.
+      $this->enitityManager->persist($entity);
+      $this->enitityManager->flush($entity);
+
+      // note the fallthrough, we show the (updated) form again.
+    }
+
+    // technical form info
+    $form = [
+      "method" => "post",
+    ];
+
+    // create rencering context
+    $context = [
+      "application" => $this->application,
+      "form" => $form,
+      "recipe" => $entity
+    ];
+
+    return $this->twig->render("recipe/edit.html.twig", $context);
+  }
+
+  public function getRecipeList()
+  {
+    return $this->repository->findAll();
   }
 }
