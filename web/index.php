9962e0fea99a06a54e6cb4a186bb634f7870daf2
Meindert-Jan Kroese <meindertjan@gmail.com>
2017-11-03 15:06:04 +0100

    moved app configuration to config.php (so we can test it later)

--- web/index.php
+++ web/index.php
@@ -5,23 +5,6 @@
 /** @var \DI\Container $container */
 $container = require_once __DIR__ . '/../index.php';
 
-$app = new \Slim\App($container);
-
-// home
-$app->get('/',
-  [\Mkroese\RecipeBook\Controller\Index::class,'getHome']);
-
-// edit/add a recipe
-$app->map(['get','post'],'/recipe/{id}/edit',
-  [\Mkroese\RecipeBook\Controller\Recipe::class,'editRecipe']);
-$app->map(['get','post'],'/recipe/add',
-  [\Mkroese\RecipeBook\Controller\Recipe::class,'editRecipe']);
-
-// delete routes
-$app->delete('/recipe/{id}',[
-  \Mkroese\RecipeBook\Controller\Recipe::class,'deleteRecipe']);
-$app->get('/recipe/{id}/delete',
-  [\Mkroese\RecipeBook\Controller\Recipe::class,'deleteRecipe']);
-
+$app = $container->get(\Slim\App::class);
 
 $app->run();
