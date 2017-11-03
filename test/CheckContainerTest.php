7dcf715c93776a5f0a98a9d4242ef68404a67aa4
Meindert-Jan Kroese <meindertjan@gmail.com>
2017-11-03 14:01:35 +0100

    added a simple test to check whether the container is initialized

---
+++ test/CheckContainerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mkroese\RecipeBook\Test;
+
+use DI\Container;
+use PHPUnit\Framework\TestCase;
+
+class CheckContainerTest extends TestCase
+{
+  public function testContainer()
+  {
+    /** @var Container $container */
+    $container = include __DIR__ . '/../index.php';
+
+    $this->assertInstanceOf(Container::class, $container,"container is not a php-di container");
+
+    $title = $container->get('application.title');
+
+    $this->assertNotEmpty($title, "application.title is empty");
+  }
+}
