70b3e73d4d9d91f475fa14fee0c1efa16f5a03ba
Meindert-Jan Kroese <meindertjan@gmail.com>
2017-11-03 19:33:08 +0100

    adapted containertest to specifically check the doctrine helperset

--- test/CheckContainerTest.php
+++ test/CheckContainerTest.php
@@ -4,20 +4,33 @@ namespace Mkroese\RecipeBook\Test;
 
 use DI\Container;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Helper\HelperSet;
 
 class CheckContainerTest extends TestCase
 {
+  /**
+   * @var Container
+   */
+  protected $container;
+
+  public function setUp() {
+    $this->container = require __DIR__.'/../index.php';
+  }
+
   public function testContainer()
   {
-    /** @var Container $container */
-    $container = include __DIR__ . '/../index.php';
+    $this->assertInstanceOf(Container::class, $this->container,
+      "container is not a php-di container");
 
-    $this->assertInstanceOf(Container::class, $container,"container is not a php-di container");
-
-    $title = $container->get('application.title');
+    $title = $this->container->get('application.title');
 
     $this->assertNotEmpty($title, "application.title is empty");
 
-    return $container;
+
+    // assert we are able to get the doctrine helperset (for the doctrine cli)
+    $helperset = $this->container->get('doctrine.orm.helperset');
+
+    $this->assertInstanceOf(HelperSet::class, $helperset,
+      'doctrine.orm.helperset is not a valid HelperSet');
   }
 }
